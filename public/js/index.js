e5e3490693d4ec7f852315d7d8364016e92f46e5
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-10-02 13:37:55 +0300

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Lauri',
-    text: 'Hey, how do you do?'
-  });
 });
 
 socket.on('disconnect', function () {
